b24f51344954cdf68c88a24b50dccf8f9d5c03b4
CristianReyes04 <cristianreyes04@hotmail.com>
2026-07-19 13:52:22 -0500

    Fix formatting of subheader in app.py

--- app.py
+++ app.py
@@ -98,8 +98,7 @@ elif modulo == "Ejercicio 3":
             presupuesto = actividad["Presupuesto"]
             resultado = calcular_interes(presupuesto,tasa,meses)
             actividad["Interés"] = resultado
-         
-      st.subheader("Lista de actividades")
+         st.subheader("Lista de actividades")
       df = pd.DataFrame(st.session_state.actividades)
       st.dataframe(df)
     
